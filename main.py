b5b010cc55df3db3fe93c586863e81549d6967e4
hbstarjason <hbstarjason@gmail.com>
2021-03-03 14:34:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -296,6 +296,30 @@ def dongaoPoints_task():
         print(traceback.format_exc())
         logging.error('【东奥积分活动】: 错误，原因为: ' + str(e))
 
+#每日1G流量日包领取
+#位置: 签到 --> 免费领 -->  免费领流量
+def dayOneG_Task():
+    try:
+        #观看视频任务
+        client.post('https://act.10010.com/SigninApp/doTask/finishVideo')
+        #请求任务列表
+        getTaskInfo = client.post('https://act.10010.com/SigninApp/doTask/getTaskInfo')
+        getTaskInfo.encoding = 'utf-8'
+        getPrize = client.post('https://act.10010.com/SigninApp/doTask/getPrize')
+        getPrize.encoding = 'utf-8'
+        client.post('https://act.10010.com/SigninApp/doTask/getTaskInfo')
+        res1 = getTaskInfo.json()
+        res2 = getPrize.json()
+        if(res1['data']['taskInfo']['status'] == '1'):
+            logging.info('【1G流量日包】: ' + res2['data']['statusDesc'])
+        else:
+            logging.info('【1G流量日包】: ' + res1['data']['taskInfo']['btn'])
+        time.sleep(1)
+    except Exception as e:
+        print(traceback.format_exc())
+        logging.error('【1G流量日包】: 错误，原因为: ' + str(e))
+
+
 #读取用户配置信息
 def readJson():
     try:
@@ -311,10 +335,13 @@ def readJson():
 def main(event, context):
     users = readJson()
     for user in users:
+        #清空上一个用户的日志记录
+        open('./log.txt',mode='w',encoding='utf-8')
         global client
         client = login.login(user['username'],user['password'],user['appId'])
         if client != False:
             daySign_task(user['username'])
+            dayOneG_Task()
             luckDraw_task()
             if ('lotteryNum' in user):
                 pointsLottery_task(user['lotteryNum'])
@@ -332,9 +359,6 @@ def main(event, context):
             notify.sendDing(user['dingtalkWebhook'])
         if ('tgToken' in user) :
             notify.sendTg(user['tgToken'],user['tgUserId'])
-        #清空上一个用户的日志记录
-        f = open('./log.txt','w')
-        f.truncate()
 
 #主函数入口
 if __name__ == '__main__':
